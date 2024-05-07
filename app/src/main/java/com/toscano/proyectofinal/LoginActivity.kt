df773c6e7b32b94ac98ead3b26d2b35647ff9f06
Jatoscano <jatoscano@uce.edu.ec>
2024-05-07 11:59:20 -0500

    Implementacion - Binding

--- app/src/main/java/com/toscano/proyectofinal/LoginActivity.kt
+++ app/src/main/java/com/toscano/proyectofinal/LoginActivity.kt
@@ -8,31 +8,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
+import com.toscano.proyectofinal.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        var edtxtUser = findViewById<EditText>(R.id.edtxt_login_user)
-        var edtxtPassword = findViewById<EditText>(R.id.edtxt_login_password)
-        var btnLogin = findViewById<Button>(R.id.btn_login_sign_in)
-        var btnSignUp = findViewById<Button>(R.id.btn_login_sign_up)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btnLogin.setOnClickListener {
-            if (edtxtUser.text.toString() == "admin" && edtxtPassword.text.toString() == "admin"){
-                Snackbar.make(edtxtUser, "Bienvenido", Snackbar.LENGTH_SHORT).show()
+        binding.btnLoginSignIn.setOnClickListener {
+            if (binding.edtxtLoginUser.text.toString() == "admin" && binding.edtxtLoginPassword.text.toString() == "admin"){
+                Snackbar.make(binding.edtxtLoginPassword, "Bienvenido", Snackbar.LENGTH_SHORT).show()
             }
 
             else{
-                Snackbar.make(edtxtUser, "Hay un error en el usuario", Snackbar.LENGTH_SHORT).show()
+                Snackbar.make(binding.edtxtLoginPassword, "Hay un error en el usuario", Snackbar.LENGTH_SHORT).show()
             }
+        }
+
+        binding.btnLoginSignUp.setOnClickListener {
+            Snackbar.make(binding.edtxtLoginUser, "Estamos en proceso", Snackbar.LENGTH_SHORT).show()
         }
     }
 }
