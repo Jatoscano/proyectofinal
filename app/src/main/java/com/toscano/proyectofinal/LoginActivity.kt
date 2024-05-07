45a06f0ddac41994bcc39910430e658f4d542451
Jatoscano <jatoscano@uce.edu.ec>
2024-05-07 11:24:43 -0500

    Iconos - Configuracion de Botones

--- app/src/main/java/com/toscano/proyectofinal/LoginActivity.kt
+++ app/src/main/java/com/toscano/proyectofinal/LoginActivity.kt
@@ -1,10 +1,13 @@
 package com.toscano.proyectofinal
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,21 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var edtxtUser = findViewById<EditText>(R.id.edtxt_login_user)
+        var edtxtPassword = findViewById<EditText>(R.id.edtxt_login_password)
+        var btnLogin = findViewById<Button>(R.id.btn_login_sign_in)
+        var btnSignUp = findViewById<Button>(R.id.btn_login_sign_up)
+
+        btnLogin.setOnClickListener {
+            if (edtxtUser.text.toString() == "admin" && edtxtPassword.text.toString() == "admin"){
+                Snackbar.make(edtxtUser, "Bienvenido", Snackbar.LENGTH_SHORT).show()
+            }
+
+            else{
+                Snackbar.make(edtxtUser, "Hay un error en el usuario", Snackbar.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/toscano/proyectofinal/SignUpActivity.kt
+++ app/src/main/java/com/toscano/proyectofinal/SignUpActivity.kt
@@ -1,10 +1,13 @@
 package com.toscano.proyectofinal
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,25 @@ class SignUpActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var edtxtName = findViewById<EditText>(R.id.edtxt_sign_up_name)
+        var edtxtLastName = findViewById<EditText>(R.id.edtxt_sign_up_last_name)
+        var edtxtEmail = findViewById<EditText>(R.id.edtxt_sign_up_email)
+        var edtxtPassword = findViewById<EditText>(R.id.edtxt_sign_up_password)
+        var btnLogin = findViewById<Button>(R.id.btn_sign_up)
+
+        btnLogin.setOnClickListener {
+            if (edtxtName.text.toString() == "" &&
+                edtxtLastName.text.toString() == "" &&
+                edtxtEmail.text.toString() == "" &&
+                edtxtPassword.text.toString() == ""){
+                Snackbar.make(edtxtName, "Hay un error en el usuario",Snackbar.LENGTH_SHORT).show()
+            }
+
+            else{
+                Snackbar.make(edtxtPassword, "Usuario ingresado correctamente", Snackbar.LENGTH_SHORT).show()
+            }
         }
     }
 }
